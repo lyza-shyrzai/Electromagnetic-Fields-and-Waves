3a6fa0718ad2c495270240a649aebd922e28abfe
LizaSh <lizash605@gmail.com>
2017-03-04 19:54:09 +0200

    Added Plot for H-sectorial antenna in the H-plane

---
+++ Horn antennas/h_h.py
@@ -0,0 +1,16 @@
+# Plot of H-sectorial antenna in the H-plane
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+angles = np.array([-35, -22, -10, 0, 32, 45, 52])
+u = np.array     ([25, 70, 10, 90, 5, 50, 20])
+
+plt.plot(angles, u)
+#plt.yscale('log')
+plt.xlabel('angles')
+plt.ylabel('u, µV')
+
+plt.grid(True)
+plt.savefig("h_h.png")
+plt.show()
