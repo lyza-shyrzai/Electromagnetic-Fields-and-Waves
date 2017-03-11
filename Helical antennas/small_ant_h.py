d432eb31dd9ca6e477ca1a2a912df8c30155524e
LizaSh <lizash605@gmail.com>
2017-03-11 20:45:30 +0200

    Fixed plot of smaller antenna in the H-plane

--- Helical antennas/small_ant_h.py
+++ Helical antennas/small_ant_h.py
@@ -6,12 +6,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-angles = np.array([-90, -80, -25, 0, 32, 63, 70])
-u = np.array     ([100, 300, 10, 500, 40, 1000, 800])
+angles = np.array([-90, -50, -25, 0, 25, 49, 90])
+u = np.array     ([100, 700, 500, 330, 125, 2000, 150])
 
 plt.plot(angles, u)
 plt.title("Plot of smaller antenna in the H-plane")
-plt.text(-95, 900, s="d(Coil diameter) = 5.5 mm \nS(distance between the coils) = 5 mm", bbox = {"boxstyle":"square", "facecolor":"w"})
+plt.text(-95, 1800, s="d(Coil diameter) = 5.5 mm \nS(distance between the coils) = 5 mm", bbox = {"boxstyle":"square", "facecolor":"w"})
 plt.xlabel('angles')
 plt.ylabel('u, µV')
 
